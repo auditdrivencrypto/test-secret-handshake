ec6ff86e1dd062b5169f1bffa26890c947fdf6e6
Laszlo Ashin <laszlo@ashin.hu>
2017-04-20 18:18:56 +0200

    Begin implementing input filtering in generator

--- generate.js
+++ generate.js
@@ -1,14 +1,66 @@
+// vim: set expandtab ts=2:
 //var JSONB = require('json-buffer')
 var fs = require('fs')
 var path = require('path')
 var map = require('./map')
+
+input_filters = {}
+input_filters.initialize = function (a) {
+  b = {
+    app_key: new Buffer(a.app_key),
+    random: new Buffer(a.random)
+  }
+  if (a.seed) b.seed = new Buffer(a.seed)
+  if (a.local) {
+    x = {}
+    if (a.local.publicKey) x.publicKey = new Buffer(a.local.publicKey)
+    if (a.local.secretKey) x.secretKey = new Buffer(a.local.secretKey)
+    if (x != {}) b.local = x
+  }
+  if (a.remote) {
+    x = {}
+    if (a.remote.publicKey) x.publicKey = new Buffer(a.remote.publicKey)
+    if (x != {}) b.remote = x
+  }
+  return b
+}
+input_filters.createChallenge = function (a) {
+  return a
+}
+input_filters.verifyChallenge = function (a) {
+  return a
+}
+input_filters.clientVerifyChallenge = function (a) {
+  return a
+}
+input_filters.clientCreateAuth = function (a) {
+  return a
+}
+input_filters.serverVerifyAuth = function (a) {
+  return a
+}
+input_filters.serverCreateAccept = function (a) {
+  return a
+}
+input_filters.clean = function (a) {
+  return a
+}
+input_filters.clientVerifyAccept = function (a) {
+  return a
+}
+input_filters.toKeys = function (a) {
+  return a
+}
 
 function wrap (crypto, output) {
 
   function wrapFn (fn, name) {
     return function () {
       var args = [].slice.call(arguments)
+      args[0] = input_filters[name](args[0])
+      var origargs = input_filters[name](args[0])
       var result = fn.apply(null, args)
+      args[0] = origargs
       output(map({name: name, args: args, result: result}, function (v) {
         //clone every object
         if(Buffer.isBuffer(v)) return new Buffer(v)
